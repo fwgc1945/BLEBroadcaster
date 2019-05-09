c2a86cfada5d3a509932f87ed722bb048e645bd7
fwgc1945 <fwgc1945@gmail.com>
2019-05-09 18:59:16 +0900

    センサー値取得ロジック作成

--- BLEBroadcaster.ino
+++ BLEBroadcaster.ino
@@ -1,47 +1,59 @@
 /*
- * File:      BLEBroadcaster.ino
- * Function:  BLEブロードキャスト通信のブロードキャスターとして動作します。
- *            設定した間隔で温湿度を計測して、宛先を問わず広く発信（ブロードキャスト）します。
- *            単一方向の送信だけで受信機能はありません。
- *            ボタンで模擬的に緊急事態の発生を通知、押すと異常発生のシグナルを送出して動作不能
- *            になります。この状態はリセットで解除されます。
- *            計測・送信時以外はディープスリープ状態に移行します。
- * Date:      2019/04/10
- * Author:    M.Ono
- * 
- * Hardware   MCU:  ESP32 (DOIT ESP32 DEVKIT V1 Board)
- *            ブレッドボードに上記開発ボードと温湿度センサー、プッシュボタン、LEDを配線
- *            温湿度センサーはDHT11 
- */
+    File:      BLEBroadcaster.ino
+    Function:  BLEブロードキャスト通信のブロードキャスターとして動作します。
+               設定した間隔で水位を計測して、宛先を問わず広く発信（ブロードキャスト）します。
+                 1.ADV_IND(iBeacon)にてアドバタイズパケットを送信
+                 2.SCAN_RSPにて水位計のデータを送信しています。 
+                 3.計測・送信時以外はディープスリープ状態に移行します。
+
+    Date:      2019/05/08
+    Author:    DENSIN Koori.A
+  
+    Hardware   MCU: ESP32 Dev Module
+               水位センサー: MaxSonar WR 
+ 
+                 MaxSonar Arduino
+                   GND GND
+                   +5V VCC
+                   TX 2
+                   RX 3
+                   AN Analog 0
+                   PW 4
+*/
+
 #include <BLEDevice.h>
 #include "BLEBeacon.h"
 #include <BLEServer.h>
 #include <esp_deep_sleep.h>
-//#include <DHT.h>                    // DHTセンサー用
 
 /* 基本属性定義  */
-//#define DEVICE_NAME "ESP32"         // デバイス名
-#define DEVICE_NAME "SkOZ"         // デバイス名
+#define DEVICE_NAME "ESP32"         // デバイス名
+//#define DEVICE_NAME "SkOZ"         // デバイス名
 #define DEVICE_NUMBER 1             // デバイス識別番号（1～8）
 #define SPI_SPEED   115200          // SPI通信速度
 #define DHTTYPE     DHT11           // DHTセンサーの型式
 
 //#define BEACON_UUID           "8ec76ea3-6668-48da-9866-75be8bc86f4d" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
-#define BEACON_UUID             "9bdf717e-591e-69ae-6743-f605d90402dc" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
+#define BEACON_UUID             "9bdf717e-591e-69ae-6743-f605d90402dc" // skeed作成の水位計（試作）のクローンになっています。
+
+//水位計属性定義
+#define normally 100    //平常時(cm)
+#define marginTop 5     //平常時の上マージン(cm)
+#define marginBottom 5  //平常時の下マージン(cm)
+#define coefficient 0.2 //係数
 
 RTC_DATA_ATTR static uint8_t seq_number;    // RTCメモリー上のシーケンス番号
+RTC_DATA_ATTR static int counter = 0;
+RTC_DATA_ATTR static float savDistance = normally;
 const int sleeping_time = 10;       // ディープスリープ時間（秒）
 const int advertising_time = 1;     // アドバータイジング時間（秒）
 
-/* DHTセンサー*/
-//const int DHTPin = 14;              // DHTセンサーの接続ピン
-//DHT   dht(DHTPin, DHTTYPE);         // DHTクラスの生成
-
 /* LEDピン */
 const int ledPin = 25;              // LEDの接続ピン
+const int ledPin2 = 26;             // LEDの接続ピン
 
-/* プッシュボタン */
-const int buttonPin = 32;           // プッシュボタンの接続ピン
+//センサー入力ピン
+const int pwPin = 4;
 
 bool bAbnormal;                     // デバイス異常判定
 
@@ -50,39 +62,78 @@ bool bAbnormal;                     // デバイス異常判定
  *****************************************************************************/
 void setup() {
     doInitialize();                           // 初期化処理をして
-    BLEDevice::init(DEVICE_NAME);             // BLEデバイスを初期化する
+    
+    //static int counter = 0;
+    //static float savDistance = normally;
 
-    int buttonState = digitalRead(buttonPin); // プッシュボタンが押されたら異常状態にする
-    if (buttonState == HIGH) {
-        bAbnormal = true;
-        Serial.println("** Abnormal condition! **");
+    Serial.print(F("\nRunning loop #")); Serial.println(counter);
+
+    float temperature = 0;
+    int distance;
+
+    // 距離を取得
+    distance = getDistance(temperature);
+
+    //変動値を求める
+    float result = distance / savDistance;
+    if (distance > savDistance)
+    {
+        result = result - 1;
     }
-    else {
-        bAbnormal = false;
+    else
+    {
+        result = 1 - result;
     }
 
-    // BLEサーバーを作成してアドバタイズオブジェクトを取得する
-    BLEServer *pServer = BLEDevice::createServer();
-    BLEAdvertising *pAdvertising = pServer->getAdvertising();
-    // 送信情報を設定してシーケンス番号をインクリメントする
-    setAdvertisementData(pAdvertising);
-    seq_number++;
+    //計測回数を加算
+    counter++;
 
-    // 所定の時間だけアドバタイズする
-    pAdvertising->start();
-    Serial.println("Advertising started!");
-    delay(advertising_time * 1000);
-    pAdvertising->stop();
+    Serial.print("distance:");
+    Serial.println(distance);
+    Serial.println(savDistance);
+    Serial.println(result);
+    Serial.print("counter:");
+    Serial.println(counter);
 
-    if (bAbnormal) {                          // 異常状態なら
-        pinMode(ledPin, OUTPUT);
-        int ledState = HIGH;
-        for (;;) {
-            digitalWrite(ledPin, ledState);    // LEDを点滅させて処理を停止する
-            ledState = !ledState;
-            delay(500);
-        }
+    //水位が前回より上がっている場合、平常時－marginTopまでは平常とする
+    //水位が前回より下がっている場合、平常時＋marginBottomまでは異常とする
+    //前回計測値より係数値分変動している場合、
+    //過去6回計測して送信していない場合に送信
+    if ((distance < savDistance  && distance <= normally - marginTop)
+        || (distance > savDistance  && distance >= normally + marginBottom)
+        || (result > coefficient)
+        || (counter >= 6)) {
+
+        // BLEデバイスを初期化する
+        BLEDevice::init(DEVICE_NAME);             
+
+        // BLEサーバーを作成してアドバタイズオブジェクトを取得する
+        BLEServer *pServer = BLEDevice::createServer();
+        BLEAdvertising *pAdvertising = pServer->getAdvertising();
+        // 送信情報を設定してシーケンス番号をインクリメントする
+        setAdvertisementData(pAdvertising, distance);
+        seq_number++;
+
+        // 所定の時間だけアドバタイズする
+        pAdvertising->start();
+        Serial.println("Advertising started!");
+        delay(advertising_time * 1000);
+        pAdvertising->stop();
+
+        //LED点減
+        digitalWrite(ledPin2, HIGH);
+        delay(500);
+        digitalWrite(ledPin2, LOW);
+
+        //計測回数をクリア
+        counter = 0;
     }
+
+    //計測値を退避
+    savDistance = distance;
+
+    Serial.println("Waiting 10 minutes...");
+
     // 外部ウェイクアップを設定してディープスリープに移行する
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_32, 1);
     Serial.println("... in deep dleep!");
@@ -95,68 +146,23 @@ void loop() {
 /*  初期化処理  */
 void doInitialize() {
     Serial.begin(SPI_SPEED);
-    pinMode(buttonPin, INPUT);        // GPIO設定：プッシュボタン        
-    //dht.begin();                      // センサーの起動
+    pinMode(pwPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
 }
 
 /*****************************< Other functions >*****************************/
 /*
   アドバタイズデータに送信情報を設定する 
 */
-void setAdvertisementData(BLEAdvertising* pAdvertising) {
-    // 温度と湿度を読み取る
-    //float t = dht.readTemperature();
-    //float h = dht.readHumidity();
-    
-    float t = 0.01;
-    float h = 0.02;
-
-
-    uint16_t temp = (uint16_t)(t * 100);
-    uint16_t humd = (uint16_t)(h * 100);
-
-    // string領域に送信情報を連結する
-    std::string strData = "";
-    strData += (char)0xff;                  // Manufacturer specific data
-    strData += (char)0xff;                  // manufacturer ID low byte
-    strData += (char)0xff;                  // manufacturer ID high byte
-    strData += (char)DEVICE_NUMBER;         // サーバー識別番号
-    strData += (char)bAbnormal;             // 異常状態判定
-    strData += (char)seq_number;            // シーケンス番号
-    //strData += (char)(temp & 0xff);         // 温度の下位バイト
-    //strData += (char)((temp >> 8) & 0xff);  // 温度の上位バイト
-    //strData += (char)(humd & 0xff);         // 湿度の下位バイト
-    //strData += (char)((humd >> 8) & 0xff);  // 湿度の上位バイト
-    strData = (char)strData.length() + strData; // 先頭にLengthを設定
-
-    //レスポンスデータをセット
-    // string領域に送信情報を連結する
-    //std::string strDataRsp = "";
-    //strDataRsp += (char)0xff;                  // Manufacturer specific data
-    //strDataRsp += (char)0xff;                  // manufacturer ID low byte
-    //strDataRsp += (char)0xff;                  // manufacturer ID high byte
-
-    //strDataRsp += (char)0x00;                  // major1
-    //strDataRsp += (char)0x65;                  // major2
-    //strDataRsp += (char)0x00;                  // minor1
-    //strDataRsp += (char)0x07;                  // minor1
-
-    //strDataRsp = (char)strDataRsp.length() + strDataRsp; // 先頭にLengthを設定
-
-    // デバイス名とフラグをセットし、送信情報を組み込んでアドバタイズオブジェクトに設定する
-    //BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
-    //oAdvertisementData.setName(DEVICE_NAME);
-    //oAdvertisementData.setFlags(0x06);      // LE General Discoverable Mode | BR_EDR_NOT_SUPPORTED
-    //oAdvertisementData.addData(strDataRsp);
-    //pAdvertising->setAdvertisementData(oAdvertisementData);
-
+void setAdvertisementData(BLEAdvertising* pAdvertising, int distance) {
 
     //iBeaconオブジェクトを設定
     BLEBeacon oBeacon = BLEBeacon();
     oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
     oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-    oBeacon.setMajor(101 & 0xFFFF);
-    oBeacon.setMinor(9 & 0xFFFF);
+    oBeacon.setMajor(101 & 0xFFFF); //電信予約値
+    oBeacon.setMinor(9 & 0xFFFF);   //距離センサー（水位含む）
     BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
 
     oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04
@@ -169,6 +175,25 @@ void setAdvertisementData(BLEAdvertising* pAdvertising) {
     oAdvertisementData.addData(strServiceData);                                                      
     pAdvertising->setAdvertisementData(oAdvertisementData);
 
+    // 水位を読み取る
+    //float t = dht.readTemperature();
+    //float h = dht.readHumidity();
+
+    float t = 0.01;
+    float h = 0.02;
+
+    uint16_t temp = (uint16_t)(t * 100);
+    uint16_t humd = (uint16_t)(h * 100);
+
+    // string領域に送信情報を連結する
+    std::string strData = "";
+    strData += (char)0xff;                  // Manufacturer specific data
+    strData += (char)0xff;                  // manufacturer ID low byte
+    strData += (char)0xff;                  // manufacturer ID high byte
+
+    strData += (char)(distance & 0xffff);   // 距離をセット
+    strData += (char)0x00;                  // ？？
+    strData = (char)strData.length() + strData; // 先頭にLengthを設定
 
     // デバイス名とフラグをセットし、送信情報を組み込んでアドバタイズオブジェクトに設定する
     BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
@@ -176,6 +201,74 @@ void setAdvertisementData(BLEAdvertising* pAdvertising) {
     oScanResponseData.setFlags(0x06);      // LE General Discoverable Mode | BR_EDR_NOT_SUPPORTED
     oScanResponseData.addData(strData);
 
+    // 作成したアドバタイズオブジェクトをResponseDataセットする
     pAdvertising->setScanResponse(true);
     pAdvertising->setScanResponseData(oScanResponseData);
+}
+
+//距離を取得
+int getDistance(float temp) {
+
+    int Duration = 0; //受信した間隔
+    int Distance = 0; //距離
+
+    int arrayDistance[8];
+    int length = 8;
+
+    for (size_t i = 0; i < length; i++) {
+
+        // パルスポートから読み込む
+        unsigned long range_pw = pulseIn(pwPin, HIGH);
+        int inch = range_pw / 147; // 147us per inch
+
+        Serial.print(inch);
+        Serial.print(" inch (");
+        Serial.print(((unsigned int)(inch) * (unsigned int)(2.54 * 64)) >> 6);
+        Serial.println(" cm) from Pulse");
+
+        if (inch > 0) {
+            Distance = ((unsigned int)(inch) * (unsigned int)(2.54 * 64)) >> 6;
+
+            // 値を配列に格納
+            arrayDistance[i] = Distance;
+        }
+
+        delay(1000);
+    }
+
+    // 数値を昇順にソート
+    int tmp;
+    for (size_t i = 0; i < length; ++i) {
+        for (size_t j = i + 1; j < length; ++j) {
+            if (arrayDistance[i] > arrayDistance[j]) {
+                tmp = arrayDistance[i];
+                arrayDistance[i] = arrayDistance[j];
+                arrayDistance[j] = tmp;
+            }
+        }
+    }
+
+    int ix1 = 0;
+    int sumDistance = 0;
+    for (size_t i = 1; i < length - 1; i++) {
+
+        // ZERO、最小値、最大値を除くサマリーを作成
+        if (arrayDistance[i] != 0) {
+            sumDistance += arrayDistance[i];
+            ix1 += 1;
+        }
+    }
+
+    Serial.print("return:");
+    Serial.println(sumDistance / ix1);
+    Serial.println(sumDistance);
+    Serial.println(ix1);
+
+    //LED点減
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+
+    // 平均値を返す
+    return sumDistance / ix1;
 }
